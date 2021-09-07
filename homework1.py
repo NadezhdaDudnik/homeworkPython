f8878f01ab5031b575d5ce9dd8fd1ca6a4a161d9
nadinQA <nadin869@mail.ru>
2021-09-07 21:48:31 +0300

    1-9 tasks are added to first homework

---
+++ homework1.py
@@ -0,0 +1,33 @@
+#first homework
+#1
+st = "20 sm"
+print('length =', st, type(st))
+#2
+a = 1
+print('a =', a, type(a))
+#3
+b = 5.6
+print('b =', b, type(b))
+#4
+byte_1 = b'6,67,8'
+print('byte_1 =', byte_1, type(byte_1))
+
+bol = True
+print('bol =', bol, type(bol))
+#5
+list_1 = [1, 2]
+print('list1 =', list_1, type(list_1))
+#6
+tuple_1 = (1, 2)
+print('tuple =', tuple_1, type(tuple_1))
+#7
+set_1 = {1, 4, 3, 6, 1}
+print('set1 =', set_1, type(set_1))
+set_2 = {'a', 'b', 'c', 'd'}
+print('set2 =', set_2, type(set_2))
+#8
+frozenset_1 = frozenset(set_1)
+print('frozenset =', frozenset_1, type(frozenset_1))
+#9
+dict_1 = {1: 'nadin', 2: 'protestinginfo'}
+print('dict =', dict_1, type(dict_1))
