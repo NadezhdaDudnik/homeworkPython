fc65a43687d9a96771854cd20b887a1f1d70fb2f
nadinqa <nadin869@mail.ru>
2022-01-27 22:18:37 +0300

    new updates

--- homework3_task3.py
+++ homework3_task3.py
@@ -1,16 +1,19 @@
 # №3 task
 def main():
-    input_money = get_money()
-    usd_money = exchange_usd(input_money)
-    eur_money = exchange_eur(input_money)
-    chf_money = exchange_chf(input_money)
-    gbp_money = exchange_gbp(input_money)
-    cny_money = exchange_cny(input_money)
-    print("Converted amount of USD-money = ", usd_money, "USD")
-    print("Converted amount of EUR-money = ", eur_money, "EUR")
-    print("Converted amount of CHF-money = ", chf_money, "CHF")
-    print("Converted amount of GBP-money = ", gbp_money, "GBP")
-    print("Converted amount of CNY-money = ", cny_money, "CNY")
+    try:
+        input_money = get_money()
+        usd_money = exchange_usd(input_money)
+        eur_money = exchange_eur(input_money)
+        chf_money = exchange_chf(input_money)
+        gbp_money = exchange_gbp(input_money)
+        cny_money = exchange_cny(input_money)
+        print("Converted amount of USD-money = ", usd_money, "USD")
+        print("Converted amount of EUR-money = ", eur_money, "EUR")
+        print("Converted amount of CHF-money = ", chf_money, "CHF")
+        print("Converted amount of GBP-money = ", gbp_money, "GBP")
+        print("Converted amount of CNY-money = ", cny_money, "CNY")
+    except ValueError as errorvalue:
+        print(errorvalue)
 
 
 def get_money():
