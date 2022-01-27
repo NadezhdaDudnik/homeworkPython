dfc6a55157fe8e41af6599f22013dffba9ff96aa
nadinqa <nadin869@mail.ru>
2022-01-27 23:30:00 +0300

    new updates

--- homework3_task3.py
+++ homework3_task3.py
@@ -34,27 +34,27 @@ def get_money():
 
 
 def exchange_usd(money):
-    int_money_usd = int(money) / 72.88
+    int_money_usd = round(int(money) / 72.88)
     return int_money_usd
 
 
 def exchange_eur(money):
-    int_money_eur = int(money) / 86.11
+    int_money_eur = round(int(money) / 86.11)
     return int_money_eur
 
 
 def exchange_chf(money):
-    int_money_chf = int(money) / 79.35
+    int_money_chf = round(int(money) / 79.35)
     return int_money_chf
 
 
 def exchange_gbp(money):
-    int_money_gbp = int(money) / 100.71
+    int_money_gbp = round(int(money) / 100.71)
     return int_money_gbp
 
 
 def exchange_cny(money):
-    int_money_cny = int(money) / 11.32
+    int_money_cny = round(int(money) / 11.32)
     return int_money_cny
 
 
